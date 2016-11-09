0f8f0cfa1056d88c53018c75262322d8dcc15bb2
Ricardo Rivas G <richistron@gmail.com>
2016-11-09 15:30:30 -0600

    documentation added for OccSearch::PaginationUrlScrapper

--- lib/occ_search/base_url.rb
+++ lib/occ_search/base_url.rb
@@ -1,3 +1,6 @@
 module OccSearch
+  ##
+  # this is being injected in other parts of the software so you are able to
+  # change this or inject other values among other classes
   BASE_URL = 'https://www.occ.com.mx'
 end
--- lib/occ_search/pagination_url_scrapper.rb
+++ lib/occ_search/pagination_url_scrapper.rb
@@ -1,19 +1,43 @@
 require "nokogiri"
 require "open-uri"
-
 module OccSearch
+  ##
+  # OccSearch::PaginationUrlScrapper is used for getting the pagination urls
+  # in a form of an array, this class uses OccBase::BASE_URL but it can be
+  # changed for mocking purpuses
   class PaginationUrlScrapper
+    ##
+    # @param <base_url> string
+    # @see OccSearch::BASE_URL
     def initialize(base_url = OccSearch::BASE_URL)
       @base_url = base_url
     end
-
+    ##
+    # finds pagination search urls
+    #
+    # @param: <url> string
+    # @return array
     def find(url)
-      page_html = get_url_content(url)
-      pages_collection = page_html.css('#numeracion_sr').css('.actual_m,.pagina_m')
-      pages = pages_collection.map { |p| p.css('a').attribute('href').text }
-      pages.map! { |p| p = (p == '#') ? url : p }
+      @nokogiri_page = get_url_content url
+      get_pages.map! { |p| p = (p == '#') ? url : p }
     end
-
+    private
+    ##
+    # @return <array> of url strings
+    def get_pages
+      nokogiri_page_links.map do |p|
+        p.css('a').attribute('href').text
+      end
+    end
+    ##
+    # @return <object> Nokogiri HTML element
+    def nokogiri_page_links
+      @nokogiri_page.css('#numeracion_sr').css('.actual_m,.pagina_m')
+    end
+    ##
+    # @param <url> string
+    # @return <Onject> Nokogiri HTML object
+    # @see http://www.nokogiri.org
     def get_url_content(url)
       Nokogiri::HTML open("#{@base_url}#{url}")
     end
