24aede2b416878a1a916358703fe41d9d84bf34c
Ricardo Rivas G <richistron@gmail.com>
2016-11-11 04:11:36 -0600

    sort

--- lib/occ_search.rb
+++ lib/occ_search.rb
@@ -1,12 +1,12 @@
-require 'occ_search/html_page'
-require 'occ_search/pagination'
-require 'occ_search/search_page'
 require 'occ_search/factories/base_url'
-require 'occ_search/url_formatter'
 require 'occ_search/factories/days'
 require 'occ_search/factories/locations'
 require 'occ_search/factories/salaries'
+require 'occ_search/html_page'
 require 'occ_search/job_page'
+require 'occ_search/pagination'
+require 'occ_search/search_page'
+require 'occ_search/url_formatter'
 
 ##
 # OccSearch module allows you to search jobs and get back ruby object with
