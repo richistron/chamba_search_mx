9c16f8b30bac309f6c8b4a0df25733797baa9166
Ricardo Rivas G <richistron@gmail.com>
2016-11-15 15:20:29 -0600

    some documentantion added

--- lib/chamba_search_mx/finder.rb
+++ lib/chamba_search_mx/finder.rb
@@ -2,6 +2,8 @@ module ChambaSearchMx
   ##
   # finds jobs and returns simplified objects of them selfs
   class Finder
+    ##
+    # @attribute <Array>
     attr_reader :jobs
     def initialize(options = {})
       @formatter = options[:formatter] || ChambaSearchMx::UrlFormatter.new
@@ -10,6 +12,10 @@ module ChambaSearchMx
       @job = options[:job] || ChambaSearchMx::JobPage.new
     end
 
+    ##
+    # @param <String> search
+    # @param <Hash> options
+    # @return <Class> self
     def find(search, options = {})
       @search = search
       @options = options
@@ -18,36 +24,50 @@ module ChambaSearchMx
       self
     end
 
+    private
+
+    ##
+    # @raise <ArgumentError>
     def validate_arguments
       error_msg = 'search must be a non empty string'
       raise ArgumentError, error_msg unless search_valid?
     end
 
+    ##
+    # @return <Boolean>
     def search_valid?
-      if @search.class != String || @search.empty?
-        false
-      else
-        true
-      end
+      return false if @search.class != String
+      return false if @search.empty?
+      true
     end
 
+    ##
+    # @return <Array>
     def find_jobs
       @jobs = job_urls.map { |url| @job.load(url).data }
     end
 
+    ##
+    # @return <Array>
     def job_urls
       pagination_urls.map { |url| @search_page.load(url).job_urls }.flatten
     end
 
+    ##
+    # @return <Array>
     def pagination_urls
       @pagination.load(format_url).urls
     end
 
+    ##
+    # @returm <string>
     def format_url
       opts = search_options
       @formatter.format @search, opts
     end
 
+    ##
+    # @return <Hash>
     def search_options
       {
         location: @options[:location] || 'default',
